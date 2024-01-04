e8823c366da463a17cbc5400899a310f25c86296
Forthtilliath <forth@live.fr>
2024-01-04 01:13:44 +0100

    feat: update use of counter + add disabled

--- src/routes/days/2/+page.svelte
+++ src/routes/days/2/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 import Button from '$lib/components/ui/button/button.svelte';
-import { createCounter } from '$lib/hooks/createCounter';
+import { createCounter } from '$lib/hooks/createCounter.svelte';
 
-let cookiesMunched = $state(0);
-const counter = createCounter((v) => (cookiesMunched = v), 0);
+const cookiesMunched = createCounter({ min: 0 });
+const disabled = $derived(cookiesMunched.value === 0);
 </script>
 
 <div
-	class="rounded-lg border bg-card text-card-foreground shadow-sm p-6 space-y-6 order-2 lg:order-1">
+	class="rounded-lg border bg-card text-card-foreground shadow-sm p-6 space-y-6 max-w-3xl m-auto">
 	<header class="space-y-1.5">
 		<h2 class="text-lg font-semibold leading-none tracking-tight">Merry Munch-o-Meter</h2>
 		<span class="mt-4 text-sm text-muted-foreground">
@@ -16,9 +16,10 @@ const counter = createCounter((v) => (cookiesMunched = v), 0);
 	</header>
 
 	<main>
-		<p class="text-5xl font-bold">{cookiesMunched}</p>
-		<Button on:click={() => counter.decrement()}>Remove</Button>
-		<Button on:click={() => counter.reset()}>Reset</Button>
-		<Button on:click={() => counter.increment()}>Add</Button>
+		<h3 class="text-lg font-semibold leading-none tracking-tight">Cookies eaten:</h3>
+		<p class="text-5xl font-bold my-2">{cookiesMunched.value}</p>
+		<Button on:click={cookiesMunched.decrement} {disabled}>Remove</Button>
+		<Button on:click={cookiesMunched.reset} {disabled}>Reset</Button>
+		<Button on:click={cookiesMunched.increment}>Add</Button>
 	</main>
 </div>
